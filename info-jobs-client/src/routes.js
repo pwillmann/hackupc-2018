c840db5f86551adfe43bbf94a19354d53802e0d0
Kolja Esders <kolja.esders@gmail.com>
2018-10-21 00:28:11 +0200

    Updated analysis page

--- info-jobs-client/src/routes.js
+++ info-jobs-client/src/routes.js
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import CvDropDown from 'pages/CvDropDownPage';
 import Analysis from 'pages/AnalysisPage';
+import Feedback from 'pages/FeedbackPage';
 
 export const Routes = () => (
   <Switch>
     <Route exact={true} path="/" component={CvDropDown} />
     <Route exact={true} path="/analysis" component={Analysis} />
+    <Route exact={true} path="/results" component={Feedback} />
   </Switch>
 );
