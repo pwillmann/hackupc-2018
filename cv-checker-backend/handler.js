c25859ff943a79e48df8ad6d8d3852a9cd9a1885
Patrick Willmann <patrick.m.willmann@gmail.com>
2018-10-20 21:57:38 +0200

    Whoop whoop

--- cv-checker-backend/handler.js
+++ cv-checker-backend/handler.js
@@ -2,6 +2,7 @@
 const ResumeParser = require("cv-parser-multiformats");
 const pdf2Text = require("pdf2text");
 const fs = require("fs");
+const rimraf = require("rimraf");
 
 if (process.env.LAMBDA_TASK_ROOT) {
   process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}/bin`;
@@ -10,8 +11,13 @@ if (process.env.LAMBDA_TASK_ROOT) {
 }
 
 module.exports.hello = async (event, context) => {
+  console.log("hello");
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
     body: JSON.stringify({
       message: "Go Serverless v1.0! Your function executed successfully!",
       input: event
@@ -19,42 +25,72 @@ module.exports.hello = async (event, context) => {
   };
 };
 
-module.exports.parsecv = async (event, context) => {
-  const file = JSON.parse(event.body);
-  console.log(file.file);
-  let rawBase64String = file.file.split(";base64,").pop();
-  console.log(rawBase64String);
-  fs.writeFileSync("/tmp/cv.pdf", rawBase64String, { encoding: "base64" });
-  const pages = await pdf2Text("/tmp/cv.pdf");
-  const text = pages.reduce((acc, val) => acc.concat(val), []).join(" ");
-  fs.writeFileSync("/tmp/cv.text", text);
+const BASE_PATH = "/tmp";
 
-  const result = await ResumeParser.parseResumeFile("/tmp/cv.text", "/tmp/") // input file, output dir
+module.exports.parsecv = async (event, context) => {
+  console.log("start parsing");
+  console.log("event: ", event);
+  const file = JSON.parse(event.body);
+  console.log("file:", file.file);
+  let rawBase64String = file.file.split(";base64,").pop();
+  const FILE_NAME = `cv_${Date.now()}`;
+  console.log("raw file:", rawBase64String);
+  fs.writeFileSync(`${BASE_PATH}/${FILE_NAME}.pdf`, rawBase64String, {
+    encoding: "base64"
+  });
+
+  const pages = await pdf2Text(`${BASE_PATH}/${FILE_NAME}.pdf`);
+  const text = pages.reduce((acc, val) => acc.concat(val), []).join(" ");
+  fs.writeFileSync(`${BASE_PATH}/${FILE_NAME}.txt`, text);
+
+  const result = await ResumeParser.parseResumeFile(
+    `${BASE_PATH}/${FILE_NAME}.txt`,
+    `${BASE_PATH}`
+  ) // input file, output dir
     .then(file => {
       console.log("Yay! " + file);
 
       console.log("\n *START* \n");
-      var content = fs.readFileSync("/tmp/cv.txt.json");
+      var content = fs.readFileSync(`${BASE_PATH}/${FILE_NAME}.txt.json`);
       console.log("Output Content : \n" + content);
       var jsonContent = JSON.parse(content);
       console.log("Name:", jsonContent.name);
       console.log("Email:", jsonContent.email);
       console.log("\n *EXIT* \n");
+
+      rimraf.sync(`${BASE_PATH}/`);
       return jsonContent;
     })
     .catch(error => {
       console.error(error);
+      rimraf.sync(`${BASE_PATH}/`);
       return error;
     });
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT"
     },
     body: JSON.stringify({
       result
     })
   };
+
+  //   this.header( 'Access-Control-Allow-Origin', options.allowOrigin );
+  this.header("Access-Control-Allow-Credentials", options.allowCredentials);
+  this.header(
+    "Access-Control-Expose-Headers",
+    headerListValue(options.exposeHeaders)
+  );
+  this.header("Access-Control-Max-Age", options.maxAge);
+  this.header(
+    "Access-Control-Allow-Headers",
+    headerListValue(options.allowHeaders)
+  );
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
