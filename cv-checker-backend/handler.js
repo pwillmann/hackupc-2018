d0669b8da9f3ed1ce1b4071b193243ac1fde4731
Patrick Willmann <patrick.m.willmann@gmail.com>
2018-10-20 23:40:39 +0200

    Find github name

--- cv-checker-backend/handler.js
+++ cv-checker-backend/handler.js
@@ -3,6 +3,7 @@ const ResumeParser = require("cv-parser-multiformats");
 const pdf2Text = require("pdf2text");
 const fs = require("fs");
 const rimraf = require("rimraf");
+const axios = require("axios");
 
 if (process.env.LAMBDA_TASK_ROOT) {
   process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}/bin`;
@@ -33,16 +34,6 @@ module.exports.parsecv = async (event, context) => {
   const file = JSON.parse(event.body);
   console.log("file:", file.file);
   let rawBase64String = file.file.split(";base64,").pop();
-<<<<<<< HEAD
-  console.log("raw file:", rawBase64String);
-  fs.writeFileSync("/tmp/cv.pdf", rawBase64String, { encoding: "base64" });
-
-  const pages = await pdf2Text("/tmp/cv.pdf");
-  const text = pages.reduce((acc, val) => acc.concat(val), []).join(" ");
-  fs.writeFileSync("/tmp/cv.txt", text);
-
-  const result = await ResumeParser.parseResumeFile("/tmp/cv.txt", "/tmp/") // input file, output dir
-=======
   const FILE_NAME = `cv_${Date.now()}`;
   console.log("raw file:", rawBase64String);
   fs.writeFileSync(`${BASE_PATH}/${FILE_NAME}.pdf`, rawBase64String, {
@@ -53,11 +44,10 @@ module.exports.parsecv = async (event, context) => {
   const text = pages.reduce((acc, val) => acc.concat(val), []).join(" ");
   fs.writeFileSync(`${BASE_PATH}/${FILE_NAME}.txt`, text);
 
-  const result = await ResumeParser.parseResumeFile(
+  const resumeJson = await ResumeParser.parseResumeFile(
     `${BASE_PATH}/${FILE_NAME}.txt`,
     `${BASE_PATH}`
   ) // input file, output dir
->>>>>>> c25859ff943a79e48df8ad6d8d3852a9cd9a1885
     .then(file => {
       console.log("Yay! " + file);
 
@@ -77,6 +67,13 @@ module.exports.parsecv = async (event, context) => {
       rimraf.sync(`${BASE_PATH}/`);
       return error;
     });
+
+  const githubName = await getGitHubUsername(resumeJson.email, resumeJson.name);
+  console.log("Found github user: ", githubName);
+  const result = {
+    ...resumeJson,
+    github: githubName
+  };
   return {
     statusCode: 200,
     headers: {
@@ -90,19 +87,84 @@ module.exports.parsecv = async (event, context) => {
       result
     })
   };
+};
 
-  //   this.header( 'Access-Control-Allow-Origin', options.allowOrigin );
-  this.header("Access-Control-Allow-Credentials", options.allowCredentials);
-  this.header(
-    "Access-Control-Expose-Headers",
-    headerListValue(options.exposeHeaders)
-  );
-  this.header("Access-Control-Max-Age", options.maxAge);
-  this.header(
-    "Access-Control-Allow-Headers",
-    headerListValue(options.allowHeaders)
-  );
+const GITHUB_USERS_API_BASE_URL = "https://api.github.com/search/users";
+const GITHUB_IN_EMAIL = "in:email";
+const GITHUB_IN_NAME = "in:fullname";
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const getGitHubUsername = async (email, fullname) => {
+  const usersByEmail = await axios.get(
+    `${GITHUB_USERS_API_BASE_URL}?q=${email}+${GITHUB_IN_EMAIL}`
+  );
+  console.log(
+    `GET:  ${GITHUB_USERS_API_BASE_URL}?q=${email}+${GITHUB_IN_EMAIL}`
+  );
+  console.log(usersByEmail.data);
+  if (
+    usersByEmail.data.items &&
+    usersByEmail.data.items[0] &&
+    usersByEmail.data.items[0].login
+  ) {
+    return usersByEmail.data.items[0].login;
+  }
+
+  const emailName = email
+    .split("@")[0]
+    .split(".")
+    .join("+");
+
+  const usersByEmailName = await axios.get(
+    `${GITHUB_USERS_API_BASE_URL}?q=${emailName}+${GITHUB_IN_EMAIL}`
+  );
+  console.log(
+    `GET:  ${GITHUB_USERS_API_BASE_URL}?q=${emailName}+${GITHUB_IN_NAME}`
+  );
+  console.log(usersByEmailName.data);
+  if (
+    usersByEmailName.data.items &&
+    usersByEmailName.data.items[0] &&
+    usersByEmailName.data.items[0].login
+  ) {
+    return usersByEmailName.data.items[0].login;
+  }
+
+  const fullNameImproved = fullname
+    .split(".")
+    .join("+")
+    .split(" ")
+    .join("+");
+  const usersByFullName = await axios.get(
+    `${GITHUB_USERS_API_BASE_URL}?q=${fullNameImproved}+${GITHUB_IN_NAME}`
+  );
+  console.log(
+    `GET:  ${GITHUB_USERS_API_BASE_URL}?q=${fullNameImproved}+${GITHUB_IN_NAME}`
+  );
+  console.log(usersByFullName.data);
+  if (
+    usersByFullName.data.items &&
+    usersByFullName.data.items[0] &&
+    usersByFullName.data.items[0].login
+  ) {
+    return usersByFullName.data.items[0].login;
+  }
+
+  const lastNameImproved = fullname.split(" ")[1];
+  const usersByLastName = await axios.get(
+    `${GITHUB_USERS_API_BASE_URL}?q=${lastNameImproved}+${GITHUB_IN_NAME}`
+  );
+  console.log(
+    `GET:  ${GITHUB_USERS_API_BASE_URL}?q=${lastNameImproved}+${GITHUB_IN_NAME}`
+  );
+  console.log(usersByLastName.data);
+
+  if (
+    usersByLastName.data.items &&
+    usersByLastName.data.items[0] &&
+    usersByLastName.data.items[0].login
+  ) {
+    return usersByLastName.data.items[0].login;
+  }
+
+  return "";
 };
