23ced68d8f77838269cc9119554f946fec124ada
Felix Boenke <felixboenke@googlemail.com>
2018-10-20 21:34:19 +0200

    Add Confirmation page

--- info-jobs-client/src/pages/ConfirmationPage/ConfirmationPage.tsx
+++ info-jobs-client/src/pages/ConfirmationPage/ConfirmationPage.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import TextField from '@material-ui/core/TextField';
+import FormGroup from '@material-ui/core/FormGroup';
 
 const Layout = styled.div`
   width: 100%;
@@ -19,13 +21,88 @@ const StyledHeader = styled.h1`
   font-weight: 700;
 `;
 
-export class ConfirmationPage extends React.Component {
+const StyledSubHeader = styled.h2`
+  margin: 0 0 3rem 0;
+  font-weight: 700;
+`;
+
+export class ConfirmationPage extends React.Component<any, any> {
   public render() {
     console.log(this.props);
     return (
       <Layout>
         <Wrapper>
           <StyledHeader>Confirm your data</StyledHeader>
+          <form>
+            <FormGroup>
+              <TextField
+                id="outlined-name"
+                label="Name"
+                margin="normal"
+                style={{ margin: 8 }}
+                variant="outlined"
+                value={
+                  this.props.location.state.result.result.name.split(' ')[0]
+                }
+              />
+              <TextField
+                id="outlined-name"
+                label="Lastname"
+                style={{ margin: 8 }}
+                margin="normal"
+                variant="outlined"
+                value={
+                  this.props.location.state.result.result.name.split(' ')[1]
+                }
+              />
+            </FormGroup>
+            <FormGroup>
+              <TextField
+                id="outlined-name"
+                label="Email"
+                style={{ margin: 8 }}
+                margin="normal"
+                variant="outlined"
+                value={this.props.location.state.result.result.email}
+              />
+            </FormGroup>
+            <FormGroup>
+              <TextField
+                id="outlined-name"
+                label="Education"
+                style={{ margin: 8 }}
+                margin="normal"
+                variant="outlined"
+                rows={3}
+                rowsMax={5}
+                multiline={true}
+                value={this.props.location.state.result.result.education}
+              />
+            </FormGroup>
+            <FormGroup>
+              <TextField
+                id="outlined-name"
+                label="Experience"
+                style={{ margin: 8 }}
+                margin="normal"
+                variant="outlined"
+                rows={3}
+                rowsMax={5}
+                multiline={true}
+                value={this.props.location.state.result.result.experience}
+              />
+            </FormGroup>
+            <StyledSubHeader>
+              Languages{' '}
+              <ul>
+                {this.props.location.state.result.result.languages
+                  .split('\n')
+                  .map(language => (
+                    <li>{language}</li>
+                  ))}
+              </ul>
+            </StyledSubHeader>
+          </form>
         </Wrapper>
       </Layout>
     );
