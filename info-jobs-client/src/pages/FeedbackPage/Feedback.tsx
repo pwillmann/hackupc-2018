194808bf58919a6a5c586e64006eb43791279572
Oruluk <pat.dein@gmail.com>
2018-10-21 12:12:06 +0200

    City

--- info-jobs-client/src/pages/FeedbackPage/Feedback.tsx
+++ info-jobs-client/src/pages/FeedbackPage/Feedback.tsx
@@ -241,10 +241,10 @@ export class Feedback extends React.Component<any, any> {
   };
 
   componentWillMount = () => {
-    this.getInfoJobOffers()
+    this.getInfoJobOffers('madrid')
   }
 
-  getInfoJobOffers = async() => {
+  getInfoJobOffers = async(city) => {
     const clientId = '6a29786cb75a4509874281c77ae4a0ca'
     const clientSecret = 'Xxj8kcjNKhUUY4ceO3ITyB3tf9V1M/eUAG1FS1Vj45ZZuNrwh3'
     const authorizationKey = 'Basic ' + btoa(clientId + ':' + clientSecret)
@@ -254,7 +254,7 @@ export class Feedback extends React.Component<any, any> {
     const PROXY_URL_PREFIX = 'https://cors-anywhere.herokuapp.com/';
     const BASE_URL = 'https://api.infojobs.net'
     const query = '/api/1/offer'
-    const queryUrl = PROXY_URL_PREFIX + BASE_URL + query
+    const queryUrl = PROXY_URL_PREFIX + BASE_URL + query + '?province=' + city
     const response = await fetch(queryUrl, {
         method: 'GET',
         headers: {'Authorization': authorizationKey,
